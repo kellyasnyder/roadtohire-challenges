5738a3e3698bfe30dda0bb40a304edb6e8de3f3a
Kelly Snyder <kellyysnyder@gmail.com>
2018-07-23 17:55:33 -0400

    adds javascript challenges

---
+++ JavaScript_1/easy.js
@@ -0,0 +1,18 @@
+// EASY: Create two variables and assign a person’s name to each. Console log a statement that says which name is shorter or longer, and by how many characters.
+// Example output: “The name Thomas is longer than John by 2 characters”
+
+var nomOne = "John"
+var nomTwo = "Jacob"
+
+function compareNames() {
+    if (nomOne.length > nomTwo.length) {
+        var difference = nomOne.length - nomTwo.length;
+        return ("The name " + nomOne + " is longer than " + nomTwo + " by " + difference + " characters");
+    }
+    else {
+        var difference = nomTwo.length - nomOne.length;
+        return ("The name " + nomTwo + " is longer than " + nomOne + " by " + difference + " characters");
+    }
+}
+
+console.log(compareNames())
---
+++ JavaScript_1/veryeasy.js
@@ -0,0 +1,11 @@
+// VERY EASY: Create two variables and assign a number to each. Console log the difference between the numbers.
+// Example output: “The difference between 6 and 2 is 4”
+
+var numOne = 13;
+var numTwo = 7;
+
+function subtractNumbers() {
+    console.log(numOne - numTwo);
+}
+
+subtractNumbers();
