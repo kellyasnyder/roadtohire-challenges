737e858252014757779123953e0a40060324eed9
Kelly Snyder <kellyysnyder@gmail.com>
2018-07-30 15:03:55 -0400

    adds completed very hard challenge

--- JavaScript_1/veryhard.js
+++ JavaScript_1/veryhard.js
@@ -2,3 +2,25 @@
 // (either +, -, * or /) and another number, and then uses the functions created in the hard challenge 
 // to output the value in sentence form.
 // Example output: "3 + 4 = 7"
+
+var numOne = prompt("Please enter a number");
+var numTwo = prompt("Please enter another number");
+var op = prompt("Please enter an operation");
+var answer;
+switch (op) {
+  case "+":
+    answer = add(parseInt(numOne), parseInt(numTwo));
+    break;
+  case "-":
+    answer = subtract(parseInt(numOne), parseInt(numTwo));
+    break;
+  case "*":
+    answer = multiply(parseInt(numOne), parseInt(numTwo));
+    break;
+  case "/":
+    answer = divide(parseInt(numOne), parseInt(numTwo));
+    break;
+  default:
+    answer = "No avalible answer";
+}
+console.log(numOne + op + numTwo + "=" + answer);
